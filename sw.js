e1093b181c8e7a953a189fe76af0550709bfd602
muraking <shige1129@nifty.com>
2026-08-12 22:41:02 +0900

    Force clients onto hierarchical filters

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rsr-meshi-v1',ASSETS=['./','./index.html','./styles.css','./app.js','./data/shops.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+const CACHE='rsr-meshi-v3',ASSETS=['./','./index.html','./styles.css?v=3','./app.js?v=3','./data/shops.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))})
